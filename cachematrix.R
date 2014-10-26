205cf7a176695da2ec69051e975b9f6de43546fb
krishna <chaitanya062@hotmail.com>
2014-10-26 23:47:08 +0530

    Assignment 2 file

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,35 @@
-## Assignment2
-makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) i<<- inverse
-  getinverse <- function() i
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
-}
-
-cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
+#Accepts matrix as input and returns functions set,get,set input and get input
+makeCacheMatrix <- function(x = matrix())
+  {
+  
+     m <- NULL
+     set <- function(y) {
+                           x <<- y
+                           m <<- NULL
+                        }
+     get <- function() x
+     setInverse <- function(Inverse) m <<- Inverse
+     getInverse <- function() 
+       
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+	 }
+#Calls makeCachematrix to compute inverse of the matrix 
+cacheSolve <- function(x, ...) 
+  {
+  
+  i <- x$getInverse()
   if(!is.null(i)) {
-    message("getting cached data.")
-    return(i)
-  }
+           
+       message("getting cached data")
+       return(i)
+          }
   data <- x$get()
-  i<- solve(data)
-  x$setinverse(inv)
-  i
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
 
 
-x=rbind(c(2,1),c(1,4))
-m=makeCacheMatrix(x)
-m$get()
+
